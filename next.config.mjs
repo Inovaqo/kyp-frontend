4feb9d4fee37e9cc3b83bab7ac1a56576b6e92fc
Mazher Hussain <mazherhussain1998@gmail.com>
2024-10-07 05:48:14 -0700

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['reactkypprofilepics.s3.ap-south-1.amazonaws.com','lh3.googleusercontent.com'],
+        unoptimized: true, // Ensure image optimization is disabled
+    },
+    reactStrictMode: false,
+};
 
 export default nextConfig;
