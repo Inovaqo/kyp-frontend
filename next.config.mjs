52ecc37ed5d31072845ac5367c050ec69e274705
Muhammad Usman Bashir <contactusmanmirza@gmail.com>
2024-10-07 18:58:14 +0500

    set strict mode false

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-
     images: {
         domains: ['reactkypprofilepics.s3.ap-south-1.amazonaws.com','lh3.googleusercontent.com'],
         unoptimized: true, // Ensure image optimization is disabled
